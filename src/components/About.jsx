12be7d68e38d14a5b0c9d1df27b51dc8adf37268
saurav10023 <sauravsonu216@gmail.com>
2026-01-21 15:25:18 +0530

    namefix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import AboutUs from "./About_components/Aboutus"
-import DirectorsMessage from "./About_components/directors_message"
+import DirectorsMessage from "./About_components/Directors_message"
 
 const About = () => {
   return (
